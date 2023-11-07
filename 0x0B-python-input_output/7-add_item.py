0acacf79e8ee28474d873cb9facc0af822437cb4
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-11-07 15:34:39 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-import random
-import sys
-from time import sleep
-import datetime
+"""
+Script that adds all arguments to a Python list, and then saves them to a file
+"""
 
-for i in range(10000):
-        sleep(random.random())
-        sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-                random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-                datetime.datetime.now(),
-                random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-                random.randint(1, 1024)
-                                                    ))
-        sys.stdout.flush()
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
